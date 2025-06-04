bbe7465a7d1b5584ef62b5a281de5865a6955a1c
Ndungu <ndungulink@gmail.com>
2025-06-04 20:40:58 +0300

    button component from shadcn

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
       <h2>Hello Ndungu</h2>
+      <Button>Subscribe</Button>
     </div>
   )
 }
